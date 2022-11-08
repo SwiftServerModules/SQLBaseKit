ad17dedeb01c5e07d16846927faec507a234573e
Vivek TOPIYA <vivektopiy12@gmail.com>
2022-11-09 00:54:49 +0530

    Updated to Dynamic Library

--- Sources/SQLBaseKit/SQL/Driver.swift
+++ Sources/SQLBaseKit/SQL/Driver.swift
@@ -3,3 +3,11 @@ public protocol Driver {
   func connect(url: String?, properties: [String: String]) -> Connection
 
 }
+open class DriverBuilder {
+  public init() {}
+
+  open func build() -> Driver {
+    fatalError("You have to override this method.")
+  }
+
+}
