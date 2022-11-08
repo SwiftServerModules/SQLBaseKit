6207a7d3021faddd52c2809a076a456019cc3d5b
Vivek TOPIYA <vivektopiy12@gmail.com>
2022-11-09 01:47:09 +0530

    Dynamic Driver Loading

---
+++ Sources/SQLBaseKit/SQL/Dialect.swift
@@ -0,0 +1,3 @@
+enum Dialect: String {
+  case mysql = "MySqlDriver"
+}
--- Sources/SQLBaseKit/SQL/DriverManager.swift
+++ Sources/SQLBaseKit/SQL/DriverManager.swift
@@ -1,12 +1,57 @@
+import Foundation
+
 public class DriverManager {
+
   init() {}
+
+  typealias InitFunction = @convention(c) () -> UnsafeMutableRawPointer
+
+  static func plugin(driverType: String, at path: String) -> Driver {
+    let openRes = dlopen(path, RTLD_NOW | RTLD_LOCAL)
+    if openRes != nil {
+      defer {
+        dlclose(openRes)
+      }
+
+      let symbolName = driverType
+      let sym = dlsym(openRes, symbolName)
+
+      if sym != nil {
+        let f: InitFunction = unsafeBitCast(sym, to: InitFunction.self)
+        let pluginPointer = f()
+        let builder = Unmanaged<DriverBuilder>.fromOpaque(pluginPointer).takeRetainedValue()
+        return builder.build()
+      } else {
+        fatalError("error loading lib: symbol \(symbolName) not found, path: \(path)")
+      }
+    } else {
+      if let err = dlerror() {
+        fatalError("error opening lib: \(String(format: "%s", err)), path: \(path)")
+      } else {
+        fatalError("error opening lib: unknown error, path: \(path)")
+      }
+    }
+  }
 
   public static func getConnection(url: String?, properties: [String: String]) -> Connection {
 
+    guard let driverType = properties["driverType"] else { fatalError("Driver type not defined") }
+    let driverInstance = loadDriverForType(driverType)
     guard let hostname: String = url else { fatalError("url is null") }
     print("Starting Database Connection to \(hostname)")
     // initilize driver here
+    return driverInstance.connect(url: url, properties: properties)
+  }
 
-    fatalError()
+  static func loadDriverForType(_ driverType: String) -> Driver {
+    let mappedDylibPath = getLibPathForDriver(driverType)
+    let aDriver = plugin(driverType: driverType, at: mappedDylibPath)
+    return aDriver
+  }
+
+  static func getLibPathForDriver(_ driverPath: String) -> String {
+    // TODO - map path against driver
+    return
+      ""
   }
 }
